12e98bd6515b54f4259e9ed0391f15d28d9b6101
SohanKP3 <sohankumarpaul23@gmail.com>
2025-02-24 23:43:18 +0530

    Add files via upload

---
+++ Day - 9/Classwork/script.js
@@ -0,0 +1,46 @@
+
+{
+    let a = 10;
+    console.log(a)
+}
+
+// +,-,*,/,%,**
+// =,+=,-=,*=,/=, **= (assigmnt operator)
+// ==, !=, !==, ===, <, >, <=, >= (comparison opertor)
+// and, or, not(Logical Operator)
+
+let a = 10, b = 10, c = 10;
+
+console.log(a+=a);
+
+//String to number if in string number present
+
+if(a===+b) {
+    console.log("a is equal to b")
+}
+else {
+    console.log("a is not equal to b")
+}
+
+function myfunction(a,b){
+    console.log(a+b);
+}
+
+c = 10;
+d = 20;
+myfunction(a,b);
+
+
+const myfunction2=(c, d) => {
+    console.log(c+d);
+}
+
+myfunction2(c,d);
+
+console.log(name);
+
+var name  = "Alexender"
+
+let k = 5;
+
+console.log(k);
---
+++ Day - 9/Classwork/script2.js
@@ -0,0 +1,23 @@
+
+let $name = "myth";
+console.log($name);
+
+myName()
+
+console.log(a)
+var a = 10;
+
+function myName() {
+    console.log(a);
+}
+
+let d = "   ";
+console.log(d.length);
+
+let k = "Raj";
+let s = "Xian";
+console.log(k,s);
+
+let f = 20;
+let m = '30';
+console.log(f+m);
